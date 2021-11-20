defd4bc2915d833e5ca0c5b4ab95dda884b0b379
rohanrudra55 <rohanrudra55@gmail.com>
2021-11-20 11:45:23 +0530

    lists updated

--- DSA/lists/multilink.h
+++ DSA/lists/multilink.h
@@ -3,7 +3,8 @@
 
 typedef struct node{
     int number;
-    int coordinates[2];
+    int coordinates_x;
+    int coordinates_y;
     struct node *row;
     struct node *column;
 }node;
@@ -18,7 +19,6 @@ void update(int,int);
 node *create_node(int,int,int);
 
 void display_sparse_mat(){
-    reset();
     printf("Matrix:\n");
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
@@ -31,7 +31,7 @@ void display_sparse_mat(){
 
 void create_upper_triangular(){
     reset();
-    node *start , *prv;
+    node *start=NULL, *prv;
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             node *new_node=NULL;
@@ -50,7 +50,6 @@ void create_upper_triangular(){
                    prv->column=new_node;
                    prv=new_node;
                }
-
             }
         }
     }
@@ -95,8 +94,8 @@ void reset(){
 node *create_node(int value,int x, int y){
     node *new_node=(node*)malloc(sizeof(node));
     new_node->number=value;
-    new_node->coordinates[0]=x;
-    new_node->coordinates[1]=y;
+    new_node->coordinates_x=x;
+    new_node->coordinates_y=y;
     new_node->column=NULL;
     new_node->row=NULL;
     return new_node;
